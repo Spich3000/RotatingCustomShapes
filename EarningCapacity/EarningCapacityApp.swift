793c6cd093a19a633df659dbb41234a9e563e7ed
Spich <spichakovda@gmail.com>
2023-04-20 20:26:53 +0300

    Initial Commit

---
+++ EarningCapacity/EarningCapacityApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarningCapacityApp.swift
+//  EarningCapacity
+//
+//  Created by Дмитрий Спичаков on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct EarningCapacityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
